0b6f2bdb13f483e91a6c47fb3fecfda3ac842dc8
RealEthanPlayzDev <30568955+RealEthanPlayzDev@users.noreply.github.com>
2021-12-28 22:52:35 +0700

    Executable build script with pyinstaller (doesn't work yet)

---
+++ make_executable.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Build emojiscraper executable
+pyinstaller --console --collect-all requests emojiscraper.py
+
+# Build stickerscraper executable
+pyinstaller --console --collect-all requests --collect-all apnggif stickerscraper.py
